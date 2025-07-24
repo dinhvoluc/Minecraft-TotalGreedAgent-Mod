6e1d8ccc828e2dfc90411e77389d64e0eb93a950
Hinoto <dinhvoluc@gmail.com>
2025-07-25 03:39:01 +0700

    ReInit

--- src/main/java/tga/TGABlocks.java
+++ src/main/java/tga/TGABlocks.java
@@ -1,8 +1,30 @@
 package tga;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 import tga.Block.BoxWood;
 
-public class TGABlocks {
-    public static BoxWood BOX_WOOD = new BoxWood(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+import java.util.function.Function;
+
+public final class TGABlocks {
+    public static Block BOX_WOOD;
+
+    public static void register() {
+        BOX_WOOD = register("box_wood", BoxWood::new, Block.Settings.create().mapColor(MapColor.TERRACOTTA_BROWN).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+    }
+    private static Block register(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        final Identifier identifier = TotalGreedyAgent.GetID(path);
+        final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
+
+        final Block block = Blocks.register(registryKey, factory, settings);
+        Items.register(block);
+        return block;
+    }
 }
--- src/main/java/tga/TotalGreedyAgent.java
+++ src/main/java/tga/TotalGreedyAgent.java
@@ -1,24 +1,20 @@
 package tga;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TotalGreedyAgent implements ModInitializer {
 	public static final String MOD_ID = "tga";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Server=>Init...");
+		TGABlocks.register();
+		LOGGER.info("Server=>Init-Ended");
+	}
+	public static Identifier GetID(String name) {
+		return Identifier.of(TGARef.Mod_ID, name);
 	}
 }
